208247aac2f6cb44ca8dbb795eda52e0ce2f54d7
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2015-06-08 13:26:36 +0530

    working version

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 (function() {
-  var b = new Blob(["this is a blob"]);
-  a = document.createElement("a")
-  a.setAttribute("href", URL.createObjectURL(b))
-  a.setAttribute('download', "awesome.txt");
-  a.innerHTML = "Download";
-  document.body.appendChild(a);
-
-  return
-
+  //"use strict";
+  //var b = new Blob(["this is a blob"]);
+  //a = document.createElement("a")
+  //a.setAttribute("href", URL.createObjectURL(b))
+  //a.setAttribute('download', "awesome.txt");
+  //a.innerHTML = "Download";
+  //document.body.appendChild(a);
+  //return
   var fileInput = document.getElementById("file");
   //returns a reader
   // function readFile(binary){
@@ -42,7 +41,32 @@
     reader.readAsBinaryString(file);
   }
 
-  function decode() {}
+  function decode() {
+
+    var reader = new FileReader(),
+    file = fileInput.files[0];
+
+    reader.onload = function() {
+      var data = reader.result;
+      var decodedData = atob(data);
+
+      var blob = new Blob([decodedData])
+      var pom = document.createElement('a');
+      //data:application/octet-stream;charset=utf-16le;base64,//5mAG8AbwAgAGIAYQByAAoA
+      pom.setAttribute('href', URL.createObjectURL(blob));
+      pom.setAttribute('download', file.name);
+
+      pom.style.display = 'none';
+      document.body.appendChild(pom);
+
+      pom.click();
+
+      document.body.removeChild(pom);
+    }
+
+    //read
+    reader.readAsText(file);
+  }
 
   //wire up the handlers
   document.getElementById("encode").addEventListener('click', encode);
