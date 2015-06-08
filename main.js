ab13b3db4d4f64ad7efd5cd5b60875f29c40c822
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2015-06-08 13:35:48 +0530

    dedup

--- main.js
+++ main.js
@@ -1,76 +1,44 @@
 (function() {
-  //"use strict";
-  //var b = new Blob(["this is a blob"]);
-  //a = document.createElement("a")
-  //a.setAttribute("href", URL.createObjectURL(b))
-  //a.setAttribute('download', "awesome.txt");
-  //a.innerHTML = "Download";
-  //document.body.appendChild(a);
-  //return
+  "use strict";
   var fileInput = document.getElementById("file");
-  //returns a reader
-  // function readFile(binary){
-  // var fileRef = fileInput.files[0],
-  // reader = new FileReader();
-  // if (!!binary){
-  // }
-  // }
-  function encode() {
+
+  function encdec(readerFn, encodeDecodeFn, filenamerFn) {
     var reader = new FileReader(),
     file = fileInput.files[0];
 
+    //gets triggered when reading is complete and the result is ready
     reader.onload = function() {
-      var data = reader.result;
-      var encodedData = btoa(data);
+      var data = reader.result,
+      decodedData = this[encodeDecodeFn](data),
+      blob = new Blob([decodedData]),
+      ael = document.createElement('a');
 
-      var blob = new Blob([encodedData])
-      var pom = document.createElement('a');
-      //data:application/octet-stream;charset=utf-16le;base64,//5mAG8AbwAgAGIAYQByAAoA
-      pom.setAttribute('href', URL.createObjectURL(blob));
-      pom.setAttribute('download', file.name + ".base64");
+      ael.setAttribute('href', URL.createObjectURL(blob));
+      ael.setAttribute('download', filenamerFn(file.name));
 
-      pom.style.display = 'none';
-      document.body.appendChild(pom);
+      ael.style.display = 'none';
+      document.body.appendChild(ael);
 
-      pom.click();
+      ael.click();
 
-      document.body.removeChild(pom);
+      document.body.removeChild(ael);
     }
 
     //read
-    reader.readAsBinaryString(file);
-  }
-
-  function decode() {
-
-    var reader = new FileReader(),
-    file = fileInput.files[0];
-
-    reader.onload = function() {
-      var data = reader.result;
-      var decodedData = atob(data);
-
-      var blob = new Blob([decodedData])
-      var pom = document.createElement('a');
-      //data:application/octet-stream;charset=utf-16le;base64,//5mAG8AbwAgAGIAYQByAAoA
-      pom.setAttribute('href', URL.createObjectURL(blob));
-      pom.setAttribute('download', file.name);
-
-      pom.style.display = 'none';
-      document.body.appendChild(pom);
-
-      pom.click();
-
-      document.body.removeChild(pom);
-    }
-
-    //read
-    reader.readAsText(file);
+    reader[readerFn](file);
   }
 
   //wire up the handlers
-  document.getElementById("encode").addEventListener('click', encode);
-  document.getElementById("decode").addEventListener('click', decode);
+  document.getElementById("encode").addEventListener('click', function() {
+    encdec('readAsBinaryString', 'btoa', function(name) {
+      return name + ".base64"
+    })
+  });
+  document.getElementById("decode").addEventListener('click', function() {
+    encdec('readAsText', 'atob', function(name) {
+      return name.replace(/\.base64$/i, '')
+    })
+  });
 
 } ())
 
