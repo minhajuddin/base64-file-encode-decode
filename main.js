9b1b809dc6baf4fede38b0caab0b45c17fcd9fae
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2015-06-18 11:23:17 +0530

    encoding working

--- main.js
+++ main.js
@@ -2,6 +2,54 @@
   "use strict";
   var fileInput = document.getElementById("file");
 
+  function binaryArrayFromBase64(encodedData) {
+    encodedData = atob(encodedData);
+    var bytes = new Array(encodedData.length);
+    for (var i = 0; i < encodedData.length; i++) {
+      bytes[i] = encodedData.charCodeAt(i);
+    }
+    return new Uint8Array(bytes);
+  }
+
+  function downloadBlob(blob, filename) {
+    var ael = document.createElement('a');
+
+    ael.setAttribute('href', URL.createObjectURL(blob));
+    ael.setAttribute('download', filename);
+
+    ael.style.display = 'none';
+    document.body.appendChild(ael);
+
+    ael.click();
+
+    document.body.removeChild(ael);
+  }
+
+  //reads the file and encodes it into a base64 text
+  function encode() {
+    var reader = new FileReader(),
+    file = fileInput.files[0];
+
+    //gets triggered when reading is complete and the result is ready
+    reader.onload = function() {
+      var data = reader.result,
+      encodedData = btoa(data),//btoa => binarytoascii (binary to base64)
+      blob = new Blob([encodedData]),
+
+      //download the file
+      ael = document.createElement('a');
+      ael.setAttribute('href', URL.createObjectURL(blob));
+      ael.setAttribute('download', file.name + ".txt");
+      ael.style.display = 'none';
+      document.body.appendChild(ael);
+      ael.click();
+      document.body.removeChild(ael);
+    }
+
+    //read
+    reader.readAsBinaryString(file);
+  }
+
   function encdec(readerFnName, encodeDecodeFn, filenamerFn) {
     var reader = new FileReader(),
     file = fileInput.files[0];
@@ -29,14 +77,10 @@
   }
 
   //wire up the handlers
-  document.getElementById("encode").addEventListener('click', function() {
-    encdec('readAsBinaryString', btoa, function(name) {
-      return name + ".base64"
-    })
-  });
+  document.getElementById("encode").addEventListener('click', encode);
   document.getElementById("decode").addEventListener('click', function() {
     encdec('readAsText', atob, function(name) {
-      return name.replace(/\.base64$/i, '')
+      return name.replace(/\.txt$/i, '')
     })
   });
 
